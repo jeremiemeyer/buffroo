b40661c899c8bc46ed6e329974441bae1cb7f54f
Jérémie Meyer <jeremie.meyer357@gmail.com>
2023-10-18 15:35:09 +0200

    layout fix

--- client/src/routes/profile.tsx
+++ client/src/routes/profile.tsx
@@ -3,9 +3,10 @@ import Title from "../components/layout/Title"
 export default function Profile() {
   return (
     <>
-      <div className="p-6">
-        <Title>Profile</Title>
+      <div className="fixed bg-white w-screen">
+        <Title className="z-[500]">Profile</Title>
       </div>
+      <div className="pt-[80px] pb-[100px] z-[0]"></div>
     </>
   )
 }
--- client/src/routes/root.tsx
+++ client/src/routes/root.tsx
@@ -1,7 +1,5 @@
 import Title from "../components/layout/Title"
-import {
-  Button,
-} from "@chakra-ui/react"
+import { Button } from "@chakra-ui/react"
 import { createPortal } from "react-dom"
 import { useState } from "react"
 import WorkoutModal from "../components/modals/Workout/WorkoutModal"
@@ -10,16 +8,26 @@ export default function Root() {
   const [showWorkoutModal, setShowWorkoutModal] = useState(false)
   return (
     <>
-      <div className="p-6">
-        <Title>Start Workout</Title>
+      <div className="fixed bg-white w-screen">
+        <Title className="z-[500]">Start Workout</Title>
+      </div>
+      <div className="pt-[80px] pb-[100px] z-[0]">
         <div className="text-xl font-semibold mt-8">Quick start</div>
-        <Button onClick={() => setShowWorkoutModal(true)} colorScheme="blue" className="mt-4">
+        <Button
+          onClick={() => setShowWorkoutModal(true)}
+          colorScheme="blue"
+          className="mt-4"
+        >
           Start an empty workout
         </Button>
 
         <div className="text-xl font-semibold mt-12">Templates</div>
       </div>
-      {showWorkoutModal && createPortal(<WorkoutModal onClose={() => setShowWorkoutModal(false)}/>, document.body)}
+      {showWorkoutModal &&
+        createPortal(
+          <WorkoutModal onClose={() => setShowWorkoutModal(false)} />,
+          document.body
+        )}
     </>
   )
 }

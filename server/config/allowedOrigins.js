d501dc622be1a60c69f413ed72e526f6f7491ffa
Jérémie Meyer <jeremie.meyer357@gmail.com>
2023-10-23 17:58:21 +0200

    test

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http://localhost:8080',
     'http://localhost:5173',
     'https://buffroo-87a1e6eff5dd.herokuapp.com/',
-    // 'https://buffroo.vercel.app'
+    'https://buffroo.vercel.app'
 ]
 
 module.exports = allowedOrigins

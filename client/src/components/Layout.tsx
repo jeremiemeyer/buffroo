f2719e32496b3f1af04b3f887c31183b04e1008f
Jérémie Meyer <jeremie.meyer357@gmail.com>
2023-10-25 14:04:14 +0200

    layout fix register/login too wide

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom"
 export default function Layout() {
   return (
     <>
-      <div className="w-screen">
+      <div className="w-screen relative">
         <Outlet />
       </div>
     </>

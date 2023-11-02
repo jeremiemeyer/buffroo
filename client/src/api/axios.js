441ee5c7a505e0854451e04342a91e89b4935b98
Jérémie Meyer <jeremie.meyer357@gmail.com>
2023-11-02 12:17:27 +0100

    mobile debug 5

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -4,6 +4,7 @@ const BASE_URL = "https://buffroo-87a1e6eff5dd.herokuapp.com/"
 
 export default axios.create({
     baseURL: BASE_URL,
+    headers: { 'Content-Type': 'application/json' },
     withCredentials: true
 })
 

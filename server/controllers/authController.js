8aa00c1ca6c270f28855f8d4f4138fbb610e1e1b
Jérémie Meyer <jeremie.meyer357@gmail.com>
2023-11-02 14:12:53 +0100

    cookie test

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -42,8 +42,8 @@ const handleLogin = async (req, res) => {
     console.log(result)
 
     res.cookie("jwt", refreshToken, {
-      httpOnly: true,  //uncomment in prod /!\
-      sameSite: "None",
+      httpOnly: true, //uncomment in prod /!\
+      // sameSite: "None",
       secure: true,
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     })
